6de9d773d5cfc7661fb17a59b06ee39942b33734
Majunbo <2388521275@qq.com>
2021-09-20 18:14:48 +0800

    hello world:加入增删改查接口

--- src/main/java/com/payrollSystem/main/HelloWorld/Controllers/HelloWorldController.java
+++ src/main/java/com/payrollSystem/main/HelloWorld/Controllers/HelloWorldController.java
@@ -1,21 +1,75 @@
 package com.payrollSystem.main.HelloWorld.Controllers;
 
 import com.payrollSystem.main.HelloWorld.Mappers.HelloWorldMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
+@Slf4j
 public class HelloWorldController {
     @Resource
     private HelloWorldMapper helloWorldMapper;
 
-    @GetMapping(value = "/hello")
     @ResponseBody
+    @GetMapping(value = "/hello")
     public List<String> hello() {
-        return helloWorldMapper.selectAllMessage();
+        List<String> result;
+        try {
+            result = helloWorldMapper.selectAllMessage();
+        } catch (Exception e) {
+            result = Collections.singletonList(e.getMessage());
+            log.error(e.getMessage());
+        }
+        return result;
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/hello/add")
+    public String helloAdd(@RequestParam(name = "msg") String helloMessage) {
+        String result;
+        try {
+            int count = helloWorldMapper.insertWithMessage(helloMessage);
+            result = "插入成功";
+        } catch (Exception e) {
+            result = "修改失败，/hello/add接口异常";
+            log.error(e.getMessage());
+        }
+        return result;
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/hello/update")
+    public String helloupdate(@RequestParam(name = "newMsg") String newMsg,
+                              @RequestParam(name = "oldMsg") String oldMsg) {
+        String result;
+        try {
+            int count = helloWorldMapper.updateWithMessage(newMsg, oldMsg);
+            result = "修改成功，共修改" + count + "行";
+        } catch (Exception e) {
+            result = "修改失败，/hello/update接口异常";
+            log.error(e.getMessage());
+        }
+        return result;
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/hello/delete")
+    public String hellodelete(@RequestParam(name = "msg") String helloMessage) {
+        String result;
+        try {
+            int count = helloWorldMapper.deleteWithMessage(helloMessage);
+            result = "删除成功，共删除" + count + "行";
+        } catch (Exception e) {
+            result = "删除失败，/hello/delete接口异常";
+            log.error(e.getMessage());
+        }
+        return result;
     }
 }
--- src/main/java/com/payrollSystem/main/HelloWorld/Mappers/HelloWorldMapper.java
+++ src/main/java/com/payrollSystem/main/HelloWorld/Mappers/HelloWorldMapper.java
@@ -1,14 +1,21 @@
 package com.payrollSystem.main.HelloWorld.Mappers;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
 public interface HelloWorldMapper {
     @Select("SELECT hello_message FROM hello_world WHERE status = 1")
-    @Result(column = "hello_message",javaType = String.class)
+    @Result(column = "hello_message", javaType = String.class)
     List<String> selectAllMessage();
+
+    @Insert("INSERT INTO hello_world (hello_message) VALUES (#{helloMessage})")
+    int insertWithMessage(String helloMessage);
+
+    @Update("UPDATE hello_world SET hello_message = #{newMsg} WHERE status = 1 AND hello_message = #{oldMsg}")
+    int updateWithMessage(String newMsg, String oldMsg);
+
+    @Update("UPDATE hello_world SET status = 0 WHERE status = 1 AND hello_message = #{helloMessage}")
+    int deleteWithMessage(String helloMessage);
 }
