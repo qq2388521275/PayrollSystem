c84b9ce7c5ffd5fdce026408884a22fbd1732bf8
Majunbo <2388521275@qq.com>
2021-09-23 14:44:04 +0800

    封装ResponseResult

--- src/main/java/com/payrollSystem/main/HelloWorld/Controllers/HelloWorldController.java
+++ src/main/java/com/payrollSystem/main/HelloWorld/Controllers/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.payrollSystem.main.HelloWorld.Controllers;
 
 import com.payrollSystem.main.HelloWorld.Mappers.HelloWorldMapper;
+import com.payrollSystem.main.ResponseResult.ResponseResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import java.util.Collections;
-import java.util.List;
 
 @Controller
 @Slf4j
@@ -19,12 +18,12 @@ public class HelloWorldController {
 
     @ResponseBody
     @GetMapping(value = "/hello")
-    public List<String> hello() {
-        List<String> result;
+    public ResponseResult hello() {
+        ResponseResult result = null;
         try {
-            result = helloWorldMapper.selectAllMessage();
+            result = ResponseResult.success(helloWorldMapper.selectAllMessage());
         } catch (Exception e) {
-            result = Collections.singletonList(e.getMessage());
+            result = ResponseResult.error(e.getMessage());
             log.error(e.getMessage());
         }
         return result;
@@ -32,13 +31,13 @@ public class HelloWorldController {
 
     @ResponseBody
     @GetMapping(value = "/hello/add")
-    public String helloAdd(@RequestParam(name = "msg") String helloMessage) {
-        String result;
+    public ResponseResult helloAdd(@RequestParam(name = "msg") String helloMessage) {
+        ResponseResult result = null;
         try {
             int count = helloWorldMapper.insertWithMessage(helloMessage);
-            result = "插入成功";
+            result = ResponseResult.success("插入成功");
         } catch (Exception e) {
-            result = "修改失败，/hello/add接口异常";
+            result = ResponseResult.error(e.getMessage());
             log.error(e.getMessage());
         }
         return result;
@@ -46,14 +45,14 @@ public class HelloWorldController {
 
     @ResponseBody
     @GetMapping(value = "/hello/update")
-    public String helloupdate(@RequestParam(name = "newMsg") String newMsg,
+    public ResponseResult helloupdate(@RequestParam(name = "newMsg") String newMsg,
                               @RequestParam(name = "oldMsg") String oldMsg) {
-        String result;
+        ResponseResult result = null;
         try {
             int count = helloWorldMapper.updateWithMessage(newMsg, oldMsg);
-            result = "修改成功，共修改" + count + "行";
+            result = ResponseResult.success("修改成功，共修改" + count + "行");
         } catch (Exception e) {
-            result = "修改失败，/hello/update接口异常";
+            result = ResponseResult.error(e.getMessage());
             log.error(e.getMessage());
         }
         return result;
@@ -61,13 +60,13 @@ public class HelloWorldController {
 
     @ResponseBody
     @GetMapping(value = "/hello/delete")
-    public String hellodelete(@RequestParam(name = "msg") String helloMessage) {
-        String result;
+    public ResponseResult hellodelete(@RequestParam(name = "msg") String helloMessage) {
+        ResponseResult result = null;
         try {
             int count = helloWorldMapper.deleteWithMessage(helloMessage);
-            result = "删除成功，共删除" + count + "行";
+            result = ResponseResult.success("删除成功，共删除" + count + "行");
         } catch (Exception e) {
-            result = "删除失败，/hello/delete接口异常";
+            result = ResponseResult.error(e.getMessage());
             log.error(e.getMessage());
         }
         return result;
---
+++ src/main/java/com/payrollSystem/main/ResponseResult/ResponseResult.java
@@ -0,0 +1,21 @@
+package com.payrollSystem.main.ResponseResult;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseResult {
+    //http状态码：200成功，500异常
+    private int status;
+    //content返回值
+    private Object content;
+
+    public static ResponseResult success(Object obj){
+        return new ResponseResult(200,obj);
+    }
+
+    public static ResponseResult error(String msg){
+        return new ResponseResult(500,msg);
+    }
+}
